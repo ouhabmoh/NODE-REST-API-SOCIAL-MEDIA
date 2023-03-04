234986cb23d773f198a6a4fe23e614312a475212
ouhab <mohamed-moh98@live.com>
2023-03-04 12:30:37 +0100

    add login and signup

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const app = express();
+
+app.listen(8000, ()=>{
+    console.log("backend server is running");
+})
